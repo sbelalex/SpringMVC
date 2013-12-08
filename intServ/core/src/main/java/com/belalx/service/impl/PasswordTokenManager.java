d777caf337c14e9f45a0da219778130e956df6d1
Aleksei Belikov <aleksei.belikov@gmail.com>
2013-12-09 01:30:52 +0300

    add all files

---
+++ intServ/core/src/main/java/com/belalx/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package com.belalx.service.impl;
+
+import com.belalx.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
